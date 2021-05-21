74ec7040ca52511d275837a47588e28702a0d224
Teun Bolkestein <teun.bolkestein@gmail.com>
2021-05-21 19:57:36 +0200

    Bot Version 1.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const Tokens = require("./Tokens/Tokens.json");
+const Tokens = require("./Tokens.json");
 
 const osu = require("node-osu");
 
@@ -21,53 +21,182 @@ bot.on("message", msg => {
     if (msg.bot == true) return;
     if (!msg.member.roles.cache.has("843898128855597156")) return;
 
+    allMaps = [
+        {
+          beatmap_name: 'Cycle Hit by - Worminators (Home Run)',
+          ids: '636839#osu/1351114'
+        },
+        {
+          beatmap_name: 'YOU AND I by - Yamakudzi (DUET)',
+          ids: '901170#osu/1881813'
+        },
+        {
+          beatmap_name: '20k (Aero Chord Remix) by - TicClick (Insane)',
+          ids: '283707#osu/641387'
+        },
+        {
+          beatmap_name: 'Headphone Actor by - Amiya (Collab Expert)',
+          ids: '936267#osu/1965897'
+        },
+        {
+          beatmap_name: 'Uta by - deetz (Collab)',
+          ids: '962562#osu/2015472'
+        },
+        {
+          beatmap_name: 'Deep In The Night by - vanucik (Insane)',
+          ids: '513194#osu/1129145'
+        },
+        {
+          beatmap_name: "Tsukinami by - Reform (SMOKELIND's Insane)",
+          ids: '896080#osu/1873147'
+        },
+        {
+          beatmap_name: 'Battle Sirens (RIOT Remix) by - Marmowka (Insane)',
+          ids: '727908#osu/1558497'
+        },
+        {
+          beatmap_name: 'Rock, Scissors, Paper by - Haya (Insane)',
+          ids: '82282#osu/229493'
+        },
+        {
+          beatmap_name: "The Monk by - FCL (Slayed's Insane)",
+          ids: '381529#osu/838063'
+        },
+        {
+          beatmap_name: "Shinzou o Sasageyo! [TV Size] by - Monstrata (Haruto's Insane)",
+          ids: '593620#osu/1256373'
+        },
+        {
+          beatmap_name: 'Sound Chimera by - Nattu (Orthrus)',
+          ids: '813569#osu/1821081'
+        },
+        {
+          beatmap_name: 'Hitorigoto -Instrumental- by - DakiniBrave (Insane)',
+          ids: '727392#osu/1535708'
+        },
+        {
+          beatmap_name: 'Holdin On (Skrillex and Nero Remix) by - Sotarks (Insane)',
+          ids: '701330#osu/1498150'
+        },
+        {
+          beatmap_name: "Spider Dance by - Fatfan Kolek (cheesiest's Light Insane)",
+          ids: '750458#osu/1580331'
+        },
+        {
+          beatmap_name: "Brain Power by - Jacob (Exote's EXHAUST)",
+          ids: '357777#osu/1017602'
+        },
+        {
+          beatmap_name: 'Tornado (Original Mix) by - Shadren (Insane)',
+          ids: '236292#osu/569636'
+        },
+        {
+          beatmap_name: 'Circles by - Peter (Insane)',
+          ids: '736339#osu/1554326'
+        },
+        {
+          beatmap_name: "Gravity Falls Theme Song by - Alphabet (Bubblun's Insane)",
+          ids: '527431#osu/1139717'
+        },
+        {
+          beatmap_name: "Packet Hero by - Fuccho (Pho's Hard)",
+          ids: '404910#osu/924562'
+        },
+        {
+          beatmap_name: "Fire (feat. Max Marshall) by - Asphyxia (Fursum's Light Insane)",
+          ids: '975615#osu/2043017'
+        },
+        {
+          beatmap_name: 'Passcode 4854 -TV edit- by - Sotarks (Insane)',
+          ids: '740068#osu/1561394'
+        },
+        {
+          beatmap_name: "Good L_ck (Yo_'re F_cked) by - Daycore (Hard)",
+          ids: '1069112#osu/2239676'
+        },
+        {
+          beatmap_name: 'ReI by - Kagetsu (Hard (feat. Logic Agent))',
+          ids: '754451#osu/1627623'
+        },
+        {
+          beatmap_name: 'Cheat Codes VIP by - xChorse (Light Insane)',
+          ids: '711420#osu/1506487'
+        },
+        {
+          beatmap_name: "Make a Move (Speed Up Ver.) by - Sotarks (naonao's Hard)",
+          ids: '765778#osu/1627149'
+        },
+        {
+          beatmap_name: 'Chameleon Love / feat.Kano by - Tari (Hard)',
+          ids: '264819#osu/603589'
+        },
+        {
+          beatmap_name: 'Snow Drive(01.23) by - Kroytz (Hard)',
+          ids: '478405#osu/1088291'
+        },
+        {
+          beatmap_name: "One Step Closer by - h4d0uk3n1 (LMT's Hard)",
+          ids: '909620#osu/1940889'
+        },
+        {
+          beatmap_name: "The Noise of Rain by - Sotarks (Shunao's Hard)",
+          ids: '859370#osu/1796153'
+        },
+        {
+          beatmap_name: 'Ano Yume o Nazotte by - Maaadbot (Hard)',
+          ids: '1192677#osu/2702114'
+        },
+        {
+          beatmap_name: "Astronomia (Medieval Style) by - Seto Kousuke (Sir Enerugi's Insane)",
+          ids: '1159452#osu/2420454'
+        },
+        {
+          beatmap_name: 'Nasty (Spor Remix) by - Shadren (Light Insane)',
+          ids: '288081#osu/652496'
+        },
+        {
+          beatmap_name: `Chirality ("Dissymmetric" Long ver.) by - Len (Akitoshi's HD)`,
+          ids: '753501#osu/1593614'
+        },
+        {
+          beatmap_name: "Kan Saete Kuyashiiwa by - Nathan (Scub's Hard)",
+          ids: '1001507#osu/2168735'
+        },
+        {
+          beatmap_name: "MONSTER by - handsome (REGRAZ'S HARD)",
+          ids: '366440#osu/819840'
+        },
+        {
+          beatmap_name: "Kako ni Torawarete Iru by - Keqing (FuJu's Hard)",
+          ids: '1245686#osu/2593879'
+        },
+        {
+          beatmap_name: "Hoohah (VIP Edit) by - Yamicchi (Plaudible's Hard)",
+          ids: '613961#osu/1295252'
+        },
+        {
+          beatmap_name: 'Overtime by - W h i t e (Hard)',
+          ids: '472143#osu/1015625'
+        },
+        {
+          beatmap_name: 'Crier by - TheShadowOfDark (Hard)',
+          ids: '974021#osu/2065919'
+        },
+        {
+          beatmap_name: 'Days to Come feat. Fiora by - Mao (Hard)',
+          ids: '1063786#osu/2315361'
+        },
+        {
+          beatmap_name: 'Ashes of the Dawn by - pw384 (Advanced)',
+          ids: '786141#osu/1701066'
+        },
+        {
+          beatmap_name: 'Rightfully by - Okoratu (Normal)',
+          ids: '875117#osu/1876170'
+        }
+    ];
+    
     msg.delete();
-
-    allMaps = {
-            'Cycle Hit by - Worminators (Home Run)': '636839#osu/1351114',
-            'YOU AND I by - Yamakudzi (DUET)': '901170#osu/1881813',
-            '20k (Aero Chord Remix) by - TicClick (Insane)': '283707#osu/641387',
-            'Headphone Actor by - Amiya (Collab Expert)': '936267#osu/1965897',
-            'Uta by - deetz (Collab)': '962562#osu/2015472',
-            'Deep In The Night by - vanucik (Insane)': '513194#osu/1129145',
-            "Tsukinami by - Reform (SMOKELIND's Insane)": '896080#osu/1873147',
-            'Battle Sirens (RIOT Remix) by - Marmowka (Insane)': '727908#osu/1558497',
-            'Rock, Scissors, Paper by - Haya (Insane)': '82282#osu/229493',
-            "The Monk by - FCL (Slayed's Insane)": '381529#osu/838063',
-            "Shinzou o Sasageyo! [TV Size] by - Monstrata (Haruto's Insane)": '593620#osu/1256373',
-            'Sound Chimera by - Nattu (Orthrus)': '813569#osu/1821081',
-            'Hitorigoto -Instrumental- by - DakiniBrave (Insane)': '727392#osu/1535708',
-            'Holdin On (Skrillex and Nero Remix) by - Sotarks (Insane)': '701330#osu/1498150',
-            "Spider Dance by - Fatfan Kolek (cheesiest's Light Insane)": '750458#osu/1580331',
-            "Brain Power by - Jacob (Exote's EXHAUST)": '357777#osu/1017602',
-            'Tornado (Original Mix) by - Shadren (Insane)': '236292#osu/569636',
-            'Circles by - Peter (Insane)': '736339#osu/1554326',
-            "Gravity Falls Theme Song by - Alphabet (Bubblun's Insane)": '527431#osu/1139717',
-            "Packet Hero by - Fuccho (Pho's Hard)": '404910#osu/924562',
-            "Fire (feat. Max Marshall) by - Asphyxia (Fursum's Light Insane)": '975615#osu/2043017',
-            'Passcode 4854 -TV edit- by - Sotarks (Insane)': '740068#osu/1561394',
-            "Good L_ck (Yo_'re F_cked) by - Daycore (Hard)": '1069112#osu/2239676',
-            'ReI by - Kagetsu (Hard (feat. Logic Agent))': '754451#osu/1627623',
-            'Cheat Codes VIP by - xChorse (Light Insane)': '711420#osu/1506487',
-            "Make a Move (Speed Up Ver.) by - Sotarks (naonao's Hard)": '765778#osu/1627149',
-            'Chameleon Love / feat.Kano by - Tari (Hard)': '264819#osu/603589',
-            'Snow Drive(01.23) by - Kroytz (Hard)': '478405#osu/1088291',
-            "One Step Closer by - h4d0uk3n1 (LMT's Hard)": '909620#osu/1940889',
-            "The Noise of Rain by - Sotarks (Shunao's Hard)": '859370#osu/1796153',
-            'Ano Yume o Nazotte by - Maaadbot (Hard)': '1192677#osu/2702114',
-            "Astronomia (Medieval Style) by - Seto Kousuke (Sir Enerugi's Insane)": '1159452#osu/2420454',
-            'Nasty (Spor Remix) by - Shadren (Light Insane)': '288081#osu/652496',
-            "Chirality ('Dissymmetric' Long ver.) by - Len (Akitoshi's HD)": '753501#osu/1593614',
-            "Kan Saete Kuyashiiwa by - Nathan (Scub's Hard)": '1001507#osu/2168735',
-            "MONSTER by - handsome (REGRAZ'S HARD)": '366440#osu/819840',
-            "Kako ni Torawarete Iru by - Keqing (FuJu's Hard)": '1245686#osu/2593879',
-            "Hoohah (VIP Edit) by - Yamicchi (Plaudible's Hard)": '613961#osu/1295252',
-            'Overtime by - W h i t e (Hard)': '472143#osu/1015625',
-            'Crier by - TheShadowOfDark (Hard)': '974021#osu/2065919',
-            'Days to Come feat. Fiora by - Mao (Hard)': '1063786#osu/2315361',
-            'Ashes of the Dawn by - pw384 (Advanced)': '786141#osu/1701066',
-            'Rightfully by - Okoratu (Normal)': '875117#osu/1876170'
-    };
 
     msg.channel.messages.fetch({limit: 50}).then(messages => {
         for (messages of messages.values()) for(const embed of messages.embeds) {
@@ -78,8 +207,9 @@ bot.on("message", msg => {
 
             ids = link.split("beatmapsets/")[1].replace(")", "");
 
-            allMaps[beatmap_name] = ids;
+            allMaps.push({beatmap_name, ids});
         }
+        console.log(allMaps);
     }).catch(console.error);
     
 
